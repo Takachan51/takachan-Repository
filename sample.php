7f4a93f8dbb473981cbd81d73db4528cf48098d9
Takachan <Takachan@Takachan-PC>
2016-09-16 02:32:27 +0900

    sample

---
+++ sample.php
@@ -0,0 +1,8 @@
+<?php
+try {
+	$pdo = new PDO('mysql:host=150.89.234.16;dbname=middle_manager;charset=utf8','root','fkmnuser',
+			array(PDO::ATTR_EMULATE_PREPARES => false));
+} catch (PDOException $e) {
+	exit('データベース接続失敗。'.$e->getMessage());
+}
+?>
