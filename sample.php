816549031900e8726bf2b790241d0d794bc900de
Takachan <Takachan@Takachan-PC>
2016-09-16 13:51:33 +0900

    sample

--- sample.php
+++ sample.php
@@ -2,9 +2,9 @@
 try {
 	$pdo = new PDO('mysql:host=150.89.234.16;dbname=middle_manager;charset=utf8','user','user',
 			array(PDO::ATTR_EMULATE_PREPARES => false));
-
+	print ("接続成功<br>");
 }catch (PDOException $e) {
 	exit('データベース接続失敗。'.$e->getMessage());
 }
-print "接続成功<br>"
+
 ?>
